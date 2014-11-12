8771f863a2de744d58079434b3c4a5564124630d
Carlos Revillo <crevillo@gmail.com>
2014-11-12 19:50:01 +0100

    initial commit

---
+++ Crevillo/Bundle/PayumRedsysBundle/CrevilloPayumRedsysBundle.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carlos
+ * Date: 31/10/14
+ * Time: 15:53
+ */
+
+namespace Crevillo\Bundle\PayumRedsysBundle;
+
+use Crevillo\Bundle\PayumRedsysBundle\DependencyInjection\Factory\Payment\RedsysPaymentFactory;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class CrevilloPayumRedsysBundle extends Bundle
+{
+    protected $name = 'PayumRedsysBundle';
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        /** @var $extension \Payum\Bundle\PayumBundle\DependencyInjection\PayumExtension */
+        $extension = $container->getExtension('payum');
+
+        $extension->addPaymentFactory(new RedsysPaymentFactory);
+    }
+}
